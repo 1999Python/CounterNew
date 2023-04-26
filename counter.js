3e4aee8c54eb5088cabe3e415530e9c4802edbb0
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-04-26 16:33:54 +0200

    changes made

--- counter.js
+++ counter.js
@@ -5,12 +5,17 @@ let myButtonDec = document.getElementById("button_decrease");
         let valueDisplay1 = document.getElementById("valueDisplay");
 
         let reset = document.getElementById("button_reset");
+
+        let multiply = document.getElementById("button_multiply");
+        
+        let text = document.getElementById("text");
         
         let counter = 0;
        
 
         myButtonDec.addEventListener("click", ()=>{
-            counter = 0;
+            counter--;
+            counter >= 0;
             valueDisplay1.innerHTML = counter;
         })
 
@@ -23,3 +28,7 @@ let myButtonDec = document.getElementById("button_decrease");
            counter = 0;
            valueDisplay1.innerHTML = counter;
         })
+        multiply.addEventListener("click", ()=>{
+            counter *=
+            valueDisplay1.innerHTML = counter;
+         })
